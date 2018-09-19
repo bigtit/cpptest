934460bdd8fd8c3bcf2a6740a79b64ff89287e75
kfang2 <kfang2@binghamton.edu>
2018-09-19 16:43:26 -0400

    change

--- variadic.cc
+++ variadic.cc
@@ -21,6 +21,7 @@ struct tsize<T>: std::integral_constant<int, sizeof(T)> {};
 
 template <int...>
 struct idxseq {};
+
 template <int N, int... Idxs>
 struct makeidx: makeidx<N-1, N-1, Idxs...> {};
 template <int... Idxs>
@@ -37,9 +38,17 @@ struct intmax: std::integral_constant<size_t, a>=intmax<r...>::value? a: intmax<
 template <size_t a>
 struct intmax<a>: std::integral_constant<size_t, a> {};
 
-// template <size_t a, size_t...>
-// struct intmax2: std::integral_constant<size_t, a> {};
 // when invoke <a1, r...>, compiler cannot fit it into <a> case???
+// since the first template for a name(class/function)
+// should be the most general one
+// while the following template with the same name would be
+// some more "specialized" ones
+// the compiler will first instantiate the "more" specialized ones
+// just like reverse direction
+// also note taht ...(three dots) can be no parameter at all
+// so the order of template with the same name should be carefully considered
+template <size_t a, size_t...>
+struct intmax2: std::integral_constant<size_t, a> {};
 template <size_t a1, size_t a2, size_t... r>
 struct intmax2<a1, a2, r...>: std::integral_constant<size_t, (a1>a2? intmax2<a1, r...>::value: intmax2<a2, r...>::value)> {};
 // template <size_t a>
@@ -51,9 +60,9 @@ int main() {
   expand(1,2,3,4);
   std::cout << tsize<int, double, char, short, int>::value << std::endl;
   using T = makeidx<4>::type;
-  std::cout << typeid(T).name() << std::endl;
+  std::cout << "makeidx " << typeid(T).name() << std::endl;
   using T2 = makeidx2<4>::type;
-  std::cout << typeid(T2).name() << std::endl;
+  std::cout << "makeidx2 " << typeid(T2).name() << std::endl;
 
   std::cout << intmax<2,5,1,7>::value << std::endl;
   std::cout << intmax2<2,5,1,7>::value << std::endl;
